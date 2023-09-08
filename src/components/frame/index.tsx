49d185793e6e05c79ed8816a9e490ab1ecaf38af
juuliana <jumaju10@hotmail.com>
2023-09-08 20:44:52 -0300

    feat: refactor frame function

--- src/components/frame/index.tsx
+++ src/components/frame/index.tsx
@@ -1,20 +1,26 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 "use client";
 import { useEffect, useState } from "react";
 
 import { Frame, frames } from "./structure";
-import { Container, Content, Item } from "./styles";
+import { Button, Container, Content, Item, Main } from "./styles";
 
 export function Frame() {
   const [data, setData] = useState([] as Frame[]);
+  const [emptyFrames, setEmptyFrame] = useState<number[]>([]);
 
   useEffect(() => {
-    increment();
-  }, []);
+    if (emptyFrames.length > 0) increment();
+  }, [emptyFrames]);
 
   function increment() {
     setData(frames);
 
-    const parsedValues = frames.map(({ values }) => values);
+    const parsedValues = frames.map(({ values, frame }) =>
+      values.map((array) => {
+        return { ...array, frame: 1 };
+      })
+    );
 
     let currentValues = [
       ...parsedValues[0],
@@ -28,14 +34,37 @@ export function Frame() {
       ...parsedValues[8],
     ];
 
-    let randomValues = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const randomValues = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
     const newData = frames.map(({ frame, values }) => {
       const newValues = values.map(({ x, y }) => {
-        const position = Math.floor(Math.random() * randomValues.length + 0);
+        const lineValues = currentValues
+          .filter((current) => current?.x === x)
+          .map(({ value }) => value)
+          .filter((value) => value);
 
-        const newValue = randomValues[position];
-        randomValues = randomValues.filter((_, index) => index !== position);
+        const columnValues = currentValues
+          .filter((current) => current?.y === y)
+          .map(({ value }) => value)
+          .filter((value) => value);
+
+        const frameValues = currentValues
+          .filter((current) => current?.frame === frame)
+          .map(({ value }) => value)
+          .filter((value) => value);
+
+        const filteredRandomValues = randomValues.filter(
+          (number: any) =>
+            !lineValues.includes(number) &&
+            !columnValues.includes(number) &&
+            !frameValues.includes(number)
+        );
+
+        const position = Math.floor(
+          Math.random() * filteredRandomValues.length + 0
+        );
+
+        const newValue = filteredRandomValues[position];
 
         const filteredValues = currentValues
           .map((current) => {
@@ -44,7 +73,10 @@ export function Frame() {
           })
           .filter((value) => value);
 
-        currentValues = [...filteredValues, { x, y, value: newValue }] as any;
+        currentValues = [
+          ...filteredValues,
+          { x, y, value: newValue, frame },
+        ] as any;
 
         return {
           x,
@@ -53,8 +85,6 @@ export function Frame() {
         };
       });
 
-      randomValues = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
       return {
         frame,
         values: newValues,
@@ -62,19 +92,33 @@ export function Frame() {
     });
 
     setData(newData as any);
+
+    const frameWithoutValues = currentValues
+      .filter((current) => !current?.value)
+      .map(({ frame }) => frame);
+
+    var filtered = frameWithoutValues.filter(
+      (value, i) => frameWithoutValues.indexOf(value) === i
+    );
+
+    setEmptyFrame(filtered);
   }
 
   return (
     <Container>
-      {data?.map(({ frame, values }) => (
-        <Content key={frame}>
-          {values?.map(({ value }) => (
-            <Item key={value}>
-              <span>{value}</span>
-            </Item>
-          ))}
-        </Content>
-      ))}
+      <Main>
+        {data?.map(({ frame, values }) => (
+          <Content key={frame}>
+            {values?.map(({ value }) => (
+              <Item key={value}>
+                <span>{value}</span>
+              </Item>
+            ))}
+          </Content>
+        ))}
+      </Main>
+
+      <Button onClick={increment}>Gerar sudoku</Button>
     </Container>
   );
 }
